3547a8faec146376439c938a22b2684a4a658eef
DuBori <92709439+DuBori@users.noreply.github.com>
2024-04-10 23:51:59 +0900

    5주차 - 관리자 상품조회

--- src/main/java/com/duboribu/ecommerce/admin/item/repository/AdminItemCustomRepositoryImpl.java
+++ src/main/java/com/duboribu/ecommerce/admin/item/repository/AdminItemCustomRepositoryImpl.java
@@ -2,9 +2,13 @@ package com.duboribu.ecommerce.admin.item.repository;
 
 import com.duboribu.ecommerce.admin.item.dto.QResponseBook;
 import com.duboribu.ecommerce.admin.item.dto.ResponseBook;
+import com.duboribu.ecommerce.admin.item.dto.SearchItemRequest;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import static com.duboribu.ecommerce.entity.QBook.book;
 import static com.duboribu.ecommerce.entity.QPrice.price;
@@ -22,5 +26,14 @@ public class AdminItemCustomRepositoryImpl implements AdminItemCustomRepository 
                 .on(book.id.eq(price.item.id))
                 .where(book.id.eq(id))
                 .fetchOne();
+    }
+
+    @Override
+    public List<ResponseBook> list(SearchItemRequest searchItemRequest, Pageable pageable) {
+        return jpaQueryFactory.select(new QResponseBook(book.id, book.title, book.author, book.publisher, price.value.intValue()))
+                .from(book)
+                .innerJoin(price)
+                .on(book.id.eq(price.item.id))
+                .fetch();
     }
 }
